2184b27da1d7bf607926e635cc83f4f73b56feba
igprasad09 <vivek87228@gmail.com>
2025-08-14 04:48:50 -0500

    vercel configaration

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from 'express';
 import AiRouter from './routes/ai.js';
 import ExeRouter from './routes/codeExecuter.js';
 import cors from 'cors';
+import dotent from 'dotenv';
+dotent.config();
+
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
@@ -10,4 +14,4 @@ app.use(cors());
 app.use('/',AiRouter);
 app.use('/code',ExeRouter);
 
-app.listen(3000, console.log("server is running.........."));
+app.listen(PORT, console.log("server is running.........."));
